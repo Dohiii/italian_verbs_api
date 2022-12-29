d1b38a73672f22745931eeebae208bdea204c708
Dohiii <val.pcclinic@gmail.com>
2022-12-29 11:41:08 +0100

    work on frontend improvements

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ async function renderFunction() {
     // console.log(osobaString)
 
     // const url = `http://127.0.0.1:3000/api/v1/verbs?categoria=${categorySelected}${charString}${osobaString}${getZwrotne}`
-    const url = `https://italian-verbs.onrender.com/api/v1/verbs?categoria=${categorySelected}${charString}${osobaString}${zwrotneValue}`
+    const url = `https://italian-verbs.onrender.com/api/v1/verbs?categoria=${categorySelected}${charString}${osobaString}${getZwrotne}`
 
     // console.log(url)
 
